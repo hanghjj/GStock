cdeee1a3834281135941acd49f415b33a7993ef0
yejinkim <yejinkim@hansol.com>
2024-04-15 16:53:27 +0900

    🎨 Design : 로그인 화면

--- src/main/gs_front/src/components/pages/Login.js
+++ src/main/gs_front/src/components/pages/Login.js
@@ -1,12 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+const LoginBlock = styled.div`
+  height: 100vh;
+  display: flex;
+  background-color: #131722;
+`;
+
+const AniBlock = styled.div`
+  width: 50%;
+`;
+
+const SignInBlock = styled.div`
+  width: 360px;
+  color: rgb(255 255 255);
+`;
+
+const FormBlock = styled.form``;
 
 const Login = () => {
   return (
-    <>
-      <div id="button">
-        <button className="main__button"></button>
-      </div>
-    </>
+    <LoginBlock>
+      <AniBlock></AniBlock>
+      <SignInBlock>
+        <h1>Sign in</h1>
+        <FormBlock></FormBlock>
+      </SignInBlock>
+    </LoginBlock>
   );
 };
 export default Login;
