5d985f9ba4b3b4dcb3fded92e95d1c4ece24899b
gunhee <hanghjj@naver.com>
2024-06-18 23:48:04 +0900

    ✨ Feat : StringUtils Empty 추가

--- src/main/java/com/gp1/gstock/common/utils/StringUtils.java
+++ src/main/java/com/gp1/gstock/common/utils/StringUtils.java
@@ -3,10 +3,13 @@ package com.gp1.gstock.common.utils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @Slf4j
 public class StringUtils {
     public static boolean isDigit(String str){
         return str.chars().allMatch(Character::isDigit);
     }
+    public static boolean isEmpty(String str) { return !Objects.nonNull(str)||!"".equals(str);}
 }
