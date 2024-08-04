b76702626715ca20cc8c068d19b27bc12f072e88
gunhee <gunhee1250@naver.com>
2024-08-04 17:03:43 +0900

    🧠 Refactor : 코인 조회 예외처리 추가

--- src/main/java/com/gp1/gstock/coin/service/impl/CoinServiceImpl.java
+++ src/main/java/com/gp1/gstock/coin/service/impl/CoinServiceImpl.java
@@ -7,9 +7,11 @@ import com.gp1.gstock.coin.entity.CoinPrice;
 import com.gp1.gstock.coin.repository.CoinPriceRepository;
 import com.gp1.gstock.coin.repository.CoinRepository;
 import com.gp1.gstock.coin.service.CoinService;
+import com.gp1.gstock.common.Exception.CustomException;
 import com.gp1.gstock.common.service.impl.ObjectMappingService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -26,20 +28,25 @@ public class CoinServiceImpl implements CoinService {
     private final ObjectMappingService mappingService;
 
     @Override
-    public CoinDto getCoin(String ticker) throws JsonProcessingException {
+    public CoinDto getCoin(String ticker) throws CustomException {
         String priceUrl = COIN_ONE_COIN_PRICE + ticker;
         String infoUrl = COIN_ONE_COIN_INFO + ticker;
         RestTemplate template = new RestTemplate();
         String priceResult = template.getForObject(priceUrl, String.class);
-        String target = new JSONObject(priceResult).getJSONArray("tickers").get(0).toString();
-        String infoResult = template.getForObject(infoUrl, String.class);
-        JSONObject currencyInfo = new JSONObject(infoResult).getJSONArray("currencies").getJSONObject(0);
-        String name = currencyInfo.getString("name");
-        String symbol = currencyInfo.getString("symbol");
-        CoinDto resultDto = mappingService.ConvertCoinOneDtoToCoinDto(target);
-        resultDto.setItmNm(name);
-        resultDto.setTicker(symbol);
-        saveCoin(resultDto);
+        CoinDto resultDto;
+        try {
+            String target = new JSONObject(priceResult).getJSONArray("tickers").get(0).toString();
+            String infoResult = template.getForObject(infoUrl, String.class);
+            JSONObject currencyInfo = new JSONObject(infoResult).getJSONArray("currencies").getJSONObject(0);
+            String name = currencyInfo.getString("name");
+            String symbol = currencyInfo.getString("symbol");
+            resultDto = mappingService.ConvertCoinOneDtoToCoinDto(target);
+            resultDto.setItmNm(name);
+            resultDto.setTicker(symbol);
+            saveCoin(resultDto);
+        } catch (JSONException | JsonProcessingException e) {
+            throw new CustomException("coin.search.fail");
+        }
         return resultDto;
     }
 
